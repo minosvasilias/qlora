9132e8e9ee4cd2e87e96260216439a443554a447
Markus <44006014+minosvasilias@users.noreply.github.com>
2023-07-31 19:00:01 +0400

    Add example configs

---
+++ scripts/finetune_llama2_7b_structured_data_mappings.sh
@@ -0,0 +1,48 @@
+python qlora.py \
+    --model_name_or_path meta-llama/Llama-2-7b-hf \
+    --use_auth \
+    --output_dir ./output/llama-2-7b-qlora \
+    --dataset ./data/llama_example_data.jsonl \
+    --dataset_format llama2 \
+    --logging_steps 10 \
+    --save_strategy steps \
+    --data_seed 42 \
+    --save_steps 200 \
+    --save_total_limit 40 \
+    --evaluation_strategy steps \
+    --eval_dataset_size 1024 \
+    --max_eval_samples 200 \
+    --per_device_eval_batch_size 1 \
+    --max_new_tokens 32 \
+    --dataloader_num_workers 1 \
+    --group_by_length \
+    --logging_strategy steps \
+    --remove_unused_columns False \
+    --do_train \
+    --do_eval \
+    --lora_r 64 \
+    --lora_alpha 16 \
+    --lora_modules all \
+    --double_quant \
+    --quant_type nf4 \
+    --bf16 \
+    --bits 4 \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type constant \
+    --gradient_checkpointing \
+    --source_max_len 3800 \
+    --target_max_len 296 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 16 \
+    --max_steps 1875 \
+    --eval_steps 900 \
+    --max_steps 1875 \
+    --eval_steps 900 \
+    --learning_rate 0.0002 \
+    --adam_beta2 0.999 \
+    --max_grad_norm 0.3 \
+    --lora_dropout 0.1 \
+    --weight_decay 0.0 \
+    --seed 0 \
+    --report_to wandb 
+    
